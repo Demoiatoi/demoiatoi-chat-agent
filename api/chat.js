92d67354c994e5c1e05e546a290eac44702dc763
Demoiatoi <gestion@demoiatoi.es>
2026-05-25 20:44:27 +0200

    Update chat.js

--- api/chat.js
+++ api/chat.js
@@ -1,7 +1,5 @@
 const { createClient } = require('@supabase/supabase-js')
 
-const { createClient } = require('@supabase/supabase-js')
-
 const supabase = createClient(
   process.env.SUPABASE_URL,
   process.env.SUPABASE_SERVICE_KEY
